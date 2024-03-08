0437f1647437e5cc2185457db552b2b4b094dca9
sriraha <161545714+sriraha@users.noreply.github.com>
2024-03-08 14:41:13 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uji_Kompetensi_Keahlian
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+                textBox1.Clear();
+            Button btn = (Button)sender;
+            textBox1.Text = textBox1.Text + btn.Text;
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            var hasil = new DataTable().Compute (a, null);
+            textBox1.Text = hasil.ToString();
+
+
+        }
+    }
+}
